cfab2572c0a0e767ca61243dd0252814fe0a1258
Sowmya Potharlanka <sowmyapotharlanka1319@gmail.com>
2026-09-04 16:02:48 +0530

    Add files via upload

---
+++ Indexing.py
@@ -0,0 +1,44 @@
+Python 3.14.4 (tags/v3.14.4:23116f9, Apr  7 2026, 14:10:54) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> #Indexing
+>>> a="vijayawada"
+>>> a[1]
+'i'
+>>> a[7]
+'a'
+>>> a[0]+a[1]+a[2]+a[3]+a[4]+a[5]
+'vijaya'
+>>> b="i am in a python class"
+>>> b[5]
+'i'
+>>> b[7]
+' '
+>>> b[9]
+' '
+>>> b[7]+b[9]
+'  '
+>>> a="i am learning python fullstack"
+>>> a[21]
+'f'
+>>> a[21]+a[22]+a[23]
+'ful'
+>>> a[21]+a[22]+a[23]+a[24]+a[25]+a[26]+a[27]+a[28]+a[29]
+'fullstack'
+>>> a[14]+a[15]+a[16]+a[17]+a[18]+a[19]
+'python'
+>>> a[5]+a[6]+a[7]+a[8]+a[9]
+'learn'
+>>> a="time is very precious"
+>>> a[-8]+a[-7]+a[-6]+a[-5]+a[-4]+a[-3]+a[-2]+a[-1]
+'precious'
+>>> a[-13]+a[-12]+a[-11]+a[-10]
+'very'
+>>> a[-21]+a[-20]+a[-19]+a[-18]
+'time'
+>>> a="codegnan it solutions"
+>>> a[-21]+a[-20]+a[-19]+a[-18]
+'code'
+>>> a[-17]+a[-16]+a[-15]+a[-14]
+'gnan'
+>>> a[-9]+a[-8]+a[-7]+a[-6]+a[-5]+a[-4]+a[-3]+a[-2]+a[-1]
+'solutions'
