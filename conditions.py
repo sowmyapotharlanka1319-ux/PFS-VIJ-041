f3488bdc97ab8c846154f4cd19e7f3638dabce5d
Sowmya Potharlanka <sowmyapotharlanka1319@gmail.com>
2026-09-12 16:02:13 +0530

    Add files via upload

---
+++ conditions.py
@@ -0,0 +1,380 @@
+#if=elif-else conditions by using comparision operators
+'''a=2
+b=4
+if a<b:
+    print('less')
+elif b>a:
+    print('greater')
+else:
+    print('true')'''
+
+
+'''a=5
+b=6
+if a>b:
+    print('less')
+elif b>a:
+    print('greater')
+else:
+    print('true')'''
+
+'''a=9
+b=12
+if a==b:
+    print('less')
+elif b<a:
+    print('greater')
+else:
+    print('true')'''
+
+'''a=3
+b=4
+if a<b:
+    print('less')
+elif b>a:
+    print('greater')
+else:
+    print('true')'''
+
+'''a=3
+b=5
+if a<b:
+    print('less')
+elif b>a:
+    print('greater')
+elif a!=b:
+    print('not equal')
+else:
+     print('true')'''
+
+#if=elif-else conditions by using logical operators
+#and,or,not
+'''a=3
+b=5
+if a!=b and b==a:
+    print('less')
+elif b>a or a<b:
+    print('greater')
+elif not a<=b:
+    print('not equal')
+else:
+     print('true')'''
+
+#if=elif-else conditions by using identify operators
+#is,is not
+'''a=17
+if type(a) is float:
+    print('less')
+elif type(a)is not int:
+    print('greater')
+else:
+     print('true')'''
+
+#if=elif-else conditions by using membership operators
+#in,not in
+'''a=[2,6,7,9,12,13,19,21,14]
+if 20 in a:
+    print('correct')
+elif 25 not in a:
+    print('ok')
+else:
+     print('true')'''
+
+#multiple-if condition by using comparision operators
+'''a=5
+b=10
+if a<b:
+    print('less')
+if b>a:
+    print('greater')
+if a!=b:
+    print('not equal')'''
+
+
+'''a=5
+b=10
+if a<b:
+    print('less')
+elif b>a:
+    print('greater')
+elif a!=b:
+    print('not equal')'''#diff blw mul-if and elif
+    
+'''a=5
+b=10
+if a==b:
+    print('less')
+if b>a:
+    print('greater')
+if a!=b:
+    print('not equal')'''
+
+#multiple-if condition by using logical operators
+#and,or,not
+'''a=5
+b=10
+if a!=b and b>a:
+    print('less')
+if a<b or b==a:
+    print('greater')
+if not a<=b:
+    print('not equal')'''
+
+#multiple-if condition by using identify operators
+#is,is not
+'''a=5
+if type(a) is int:
+    print('correct')
+if type(a) is not float:
+    print('true')'''
+
+#multiple-if condition by using membership operators
+#in,not in
+'''a=[2,3,5,6,8,9]
+if 8 in a:
+    print('ok')
+if 9 not in a:
+    print('not ok')'''
+
+#nested-if condition by using comparision operators
+'''a=6
+b=12
+if a<b:
+    print('less')
+    if b>a:
+        print('greater')'''
+
+
+'''a=6
+b=12
+if a>b:
+    print('less')
+if b>a:
+    print('greater')'''#diff b/w mul-if and nested-if
+
+
+'''a=6
+b=12
+if a==b:
+    print('less')
+    if b>a:
+        print('greater')'''
+
+'''a=12
+b=21
+if a<b:
+    print('less')
+    if b==a:
+        print('equal')'''
+
+'''a=6
+b=12
+if a<b:
+    print('less')
+    if b==a:
+        print('greater')
+    else:
+        print('true')'''
+
+'''
+a=30
+b=60
+if a>b:
+    print('less')
+    if b>a:
+        print('equal')
+else:
+    print('true')'''
+
+
+'''a=60
+b=80
+if a<b:
+    print('less')
+    if b>a:
+        print('greater')
+    else:
+        print('false')
+else:
+    print('true')'''
+
+'''a=60
+b=80
+if a>b:
+    print('less')
+    if b==a:
+        print('equal')
+    else:
+        print('false')
+else:
+    print('true')'''
+
+'''a=60
+b=80
+if a<b:
+    print('less')
+    if b==a:
+        print('equal')
+    elif a!=b:
+        print('not equal')
+    else:
+        print('false')
+else:
+    print('true')'''
+
+#nested-if using logical operators
+#and,or,not
+'''a=60
+b=80
+if a<b and a==b:
+    print('less')
+    if b>a or a<=b:
+        print('greater')
+    else:
+        print('false')
+elif not a==b:
+    print('okay')
+else:
+    print('true')'''
+
+#nested-if using identify operators
+#is,is not
+'''a=19.83
+if type(a) is int:
+     print('correct')
+     if type(a)is not float:
+         print('not ok')
+     elif type(a)is int:
+         print('false')
+else:
+    print('true')'''
+
+
+#nested-if using membership operators
+#in,not in
+'''a=[1,4,5,9,7,12,22,34,56,77,65,92]
+if 12 in a:
+    print('correct')
+    if 22 not in a:
+        print('false')
+    elif 34 in a:
+        print('okay cool')
+elif 24 not in a:
+    print('true')
+else:
+    print('ok')'''
+ 
+
+
+
+
+
+
+
+
+
+    
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+    
