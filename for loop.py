a061e929ee47c56a4f7b0ab2ce8d4fcd257cf2af
Sowmya Potharlanka <sowmyapotharlanka1319@gmail.com>
2026-09-17 16:06:46 +0530

    Add files via upload

---
+++ for loop.py
@@ -0,0 +1,120 @@
+#for loop
+'''a=[10,20,30,40]
+for i in a:
+    print(i)'''
+
+'''a=[10,20,30,40]
+for i in a:
+    print(a)'''#iterates upto size of set a
+    
+'''a=[10,20,30,40]
+for i in a:
+    print(i)
+print(type(a))
+print(type(i))'''
+
+'''a=[10,20,30,40,88,20,30,50]
+for i in a:
+    print(i)
+print(type(a))
+print(type(i))'''
+
+'''a={"year":2026,"month":"Sep","date":16}
+for i in a:
+    print(i)
+for i in a.keys():
+    print(i)
+print(type(a))
+print(type(i))
+for i in a.values():
+    print(i)
+print(type(a))
+print(type(i))
+for i in a.items():
+    print(i)
+print(type(a))
+print(type(i))'''
+
+'''a=[4.5,6.8,8.00]
+for i in a:
+    print(i)
+print(type(a))
+print(type(i))'''
+
+'''a=["python","java","c"]
+for i in a:
+    print(i)
+print(type(a))
+print(type(i))'''
+
+'''a=["python","java","c"]
+for i in a:
+    print(i,end=" ")
+print(type(a))
+print(type(i))'''
+
+'''a=[4+7j,7+2j]
+for i in a:
+    print(i)
+print(type(a))
+print(type(i))'''
+
+'''a=[True,False]
+for i in a:
+    print(i)
+print(type(a))
+print(type(i))'''
+
+'''a=[2,4.5,"python",4+8j,True]
+for i in a:
+    print(i)
+print(type(a))
+print(type(i))'''
+
+#Task
+'''a=['apple','banana','grapes']
+for i in a:
+  print(i.upper(),end=" ")'''
+
+#m-2
+'''b=str(a)
+print(b.upper())'''
+
+#m-3    
+'''b=[]
+for i in a:
+  b.append(i.upper())
+print(b)'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+       
---
+++ while ,range loops.py
@@ -0,0 +1,224 @@
+#while loop
+'''a=10
+while a>1:
+    print(a)'''
+
+'''a=10
+while a>1:
+    print(a)'''
+    
+'''a=20
+while a>=1:
+    print(a)
+    a=a-1'''
+    
+'''a=20
+while a>=1:
+    a=a-1
+    print(a)'''
+
+'''a=20
+while a>1:
+    a=a-1
+print(a)'''
+
+
+'''a=30
+while a>=1:
+    print(a)
+    a+=1'''
+    
+'''a=30
+while a>=1:
+    print(a)
+    a-=1'''
+
+'''a=5
+while a<15:
+    print(a)
+    a+=1'''
+
+#voting using while loop
+'''while True:
+    age=int(input('enter the age:'))
+    if age>=18:
+        print('eligible to vote')
+    else:
+         print('not eligible')'''
+
+#leap & nonleap year
+'''while True:
+    y=int(input('enter the year'))
+    if y%4==0:
+        print('leap year')
+    else:
+        print('non-leap year')'''
+
+
+#3rd loop: range()
+#start-stop-step
+'''for i in range(10):#by default it starts from '0'
+    print(i)'''
+
+'''for i in range(15,30):
+    print(i)'''
+
+#tasks on range()
+#0,2,4,6,8,10,12,14,16,18
+'''for i in range(0,20,2):
+    print(i,end=",")'''
+
+'''for i in range(5,50,5):
+    print(i,end=",")'''
+
+
+'''for i in range(3,30,3):
+    print(i,end=",")'''
+
+
+
+#tasks using loops
+#grade code
+'''while True:
+    marks=int(input('enter marks:'))
+    if marks in range(91,101):
+            print('Grade-A')
+    elif marks in range(81,91):
+            print('Grade-B')
+    elif marks in range(71,81):
+            print('Grade-C')
+    elif marks in range(50,71):
+            print('Grade-D')
+    else:
+            print('Fail')'''
+
+
+#attendence tracker code
+students=['p',a','p','p','a']
+present=0
+absent=0
+for status in students:
+    if status=='p':
+        present+=1
+    else:
+        absent+=1
+print('.................ATTENDENCE TRACKER................')
+print('total no of students:',len(n))
+print('presentees:',present)
+print('absentees:',absent)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
